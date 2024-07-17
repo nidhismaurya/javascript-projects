14b5797255d1df39688c49910beeda01968a6119
nidhismaurya <mauryas.nidhi@gmail.com>
2024-07-17 16:36:15 -0400

    moreOnFunctions

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -4,5 +4,13 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
+nums2.sort(function(x, y) {
+    return x-y;
+});
+console.log(nums2);
 
 //Sort each array in descending order.
+nums2.sort(function(x, y) {
+    return y-x;
+});
+console.log(nums2);
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -10,6 +10,16 @@ function findMinValue(arr){
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
+function sortNums(arr) {
+  let sorted = [];
+  while (arr.lenth) {
+  let min = findMinValue(arr)
+  sorted.push(min);
+  arr.splice(arr.indexOf(min), 1)
+  }
+return sorted;
+}
+
 /*Within the function:
 1) Define a new, empty array to hold the final sorted numbers.
 2) Use the findMinValue function to find the minimum value in the old array.
@@ -27,3 +37,4 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+console.log(sortNums(nums3));
